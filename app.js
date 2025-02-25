2f4501437f66e5d5e34c1438f63e3b72fcb4b956
diectorsszone <mail.groupofsszone@gmail.com>
2025-02-25 17:10:42 +0530

    updated

---
+++ app.js
@@ -0,0 +1,19 @@
+let bx2= document.getElementById("m2");
+let aud= document.getElementById("audio");
+// window.addEventListener("DOMContentLoaded", () => {
+    
+//     aud.muted = true; // Mute karna zaroori hai
+//     aud.play();
+//   });
+const audio = document.getElementById("myAudio");
+
+
+function inc_height() {
+    let bx_ht=parseFloat(window.getComputedStyle(bx2).height);
+    bx2.style.height=bx_ht+100+"px"
+}
+let student =[{name:"ram",age:26,add:"ptn"},{name:"mohan",age:36,add:"dbg"},{name:"aman",age:26,add:"skp"}]
+
+let std2= {ajay:{name:"ajay",age:45,add:"lagma"}}
+
+
